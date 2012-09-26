3f2158630a2eb1e060938ca89d80572969156789
Merijn de Jonge <merijn.de.jonge@gmail.com>
2012-09-26 12:21:40 -0700

    FluentMetadata: Added RoundTripOriginal

--- FluentMetadata/FluentConfiguration/MemberProjectionMetadata.cs
+++ FluentMetadata/FluentConfiguration/MemberProjectionMetadata.cs
@@ -1,11 +1,8 @@
-﻿// MemberProjectionMetadata.cs
-//
+﻿using System.ComponentModel.DataAnnotations;
+using System.ServiceModel.DomainServices.Server;
 
 namespace System.Web.DomainServices.FluentMetadata
 {
-    using System.ComponentModel.DataAnnotations;
-    using System.ServiceModel.DomainServices.Server;
-
     public class MemberProjectionMetadata<TEntity, TMember>
         where TEntity : class
     {
@@ -31,10 +28,7 @@ namespace System.Web.DomainServices.FluentMetadata
 
         public MetadataClass<TEntity> Metadata
         {
-            get
-            {
-                return _metadata;
-            }
+            get { return _metadata; }
         }
 
         #endregion
@@ -42,8 +36,8 @@ namespace System.Web.DomainServices.FluentMetadata
         #region Public Methods and Operators
 
         /// <summary>
-        /// Indicates that an entity member should not exist in the code generated client
-        /// view of the entity, and that the value should never be sent to the client.
+        ///   Indicates that an entity member should not exist in the code generated client
+        ///   view of the entity, and that the value should never be sent to the client.
         /// </summary>
         public void Exclude()
         {
@@ -51,7 +45,7 @@ namespace System.Web.DomainServices.FluentMetadata
         }
 
         /// <summary>
-        /// Indicates that the associated entity should be included.
+        ///   Indicates that the associated entity should be included.
         /// </summary>
         public void Include()
         {
@@ -59,20 +53,30 @@ namespace System.Web.DomainServices.FluentMetadata
         }
 
         /// <summary>
-        /// Indicates that the associated entity should be included.
+        ///   Indicates that the associated entity should be included.
         /// </summary>
-        /// <param name="memberPath">Dotted path specifying the navigation path from the member this attribute
-        ///     is applied to, to the member to be projected. The projected member must be
-        ///     a scalar.</param>
-        /// <param name="name">The member name for the projected member.</param>
+        /// <param name="memberPath"> Dotted path specifying the navigation path from the member this attribute is applied to, to the member to be projected. The projected member must be a scalar. </param>
+        /// <param name="name"> The member name for the projected member. </param>
         public void IncludeMember(string memberPath, string name)
         {
             _metadata.AddMetadata(MemberName, new IncludeAttribute(memberPath, name));
         }
 
+        /// <summary>
+        ///   Denotes one or more properties that uniquely identify an entity.
+        /// </summary>
         public void IsKey()
         {
             _metadata.AddMetadata(MemberName, new KeyAttribute());
+        }
+
+        /// <summary>
+        ///   Indicates that the original value
+        ///   of the member should be sent back to the server when the object is updated.
+        /// </summary>
+        public void RoundTripOriginal()
+        {
+            _metadata.AddMetadata(MemberName, new RoundtripOriginalAttribute());
         }
 
         #endregion
